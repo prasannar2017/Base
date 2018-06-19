c651abe1c6015a05f3e6a68d3f0e26a056abbcc4
Prasanna Aithal <prasanna.aithal@tapzo.com>
2018-06-19 17:23:16 +0530

    Cab added

---
+++ Cab.framework/Headers/Cab.h
@@ -0,0 +1,19 @@
+//
+//  Cab.h
+//  Cab
+//
+//  Created by Prasanna Aithal on 19/06/18.
+//  Copyright © 2018 Prasanna Aithal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cab.
+FOUNDATION_EXPORT double CabVersionNumber;
+
+//! Project version string for Cab.
+FOUNDATION_EXPORT const unsigned char CabVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cab/PublicHeader.h>
+
+
